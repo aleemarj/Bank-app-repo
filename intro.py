7b6a2e9503fba9280dc52ea4a0e73d97483e1658
aleemarj <jamilaaminu805@gmail.com>
2025-03-01 10:51:36 +0100

    Initial Commit

---
+++ intro.py
@@ -0,0 +1,45 @@
+welcome_message = 'The lady said : "come inside the classroom!"'
+print(welcome_message)
+welcome_message = 'The man said : "Bring in Jason\s"'
+print(5<4)
+List_of_fruits= ["Apple", "Banana", "Pawpaw"]
+print(List_of_fruits)
+mixed_list=[1,2.3, list_of_fruits]
+
+name = input("Enter your name:")
+surname = input("Enter your name:")
+
+joined = name + surname
+count_name = len(joined)
+output = f"The number of characters in the name {name} {surname} is {count_name}"
+print(output)
+
+age="67"+"8"
+print(age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
